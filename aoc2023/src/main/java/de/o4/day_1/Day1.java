71a71182712080bda3d01b4c95903dbf503ea224
Oliver Vier <119545999+OliverVier@users.noreply.github.com>
2023-12-01 16:13:37 +0100

    finished part 2

--- aoc2023/src/main/java/de/o4/day_1/Day1.java
+++ aoc2023/src/main/java/de/o4/day_1/Day1.java
@@ -1,7 +1,10 @@
 package de.o4.day_1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import de.o4.utils.TextReader;
 
@@ -11,6 +14,7 @@ public class Day1 {
         Day1 main = new Day1();
         List<String> output = TextReader.read("aoc2023\\src\\main\\java\\de\\o4\\day_1\\puzzle_input_1.txt");
         System.out.println(main.findNumbers(output).stream().reduce(0, (n1, n2) -> n1 + n2));
+        System.out.println(main.findNumbersPart2(output).stream().reduce(0, (n1, n2) -> n1 + n2));
     }
 
     public List<Integer> findNumbers(List<String> output) {
@@ -23,11 +27,48 @@ public class Day1 {
                     numberCharacters.add(c);
                 }
             }
-            int number = Integer
-                    .valueOf(numberCharacters.get(0) + "" + numberCharacters.get(numberCharacters.size() - 1));
+            
+            int number;
+            if(numberCharacters.size() > 1){
+                number = Integer.valueOf(numberCharacters.get(0) + "" + numberCharacters.get(numberCharacters.size() - 1));
+            } else{
+                number = 0;
+            }
+
             numbers.add(number);
         }
 
+        return numbers;
+    }
+
+    public List<Integer> findNumbersPart2(List<String> output) {
+
+        HashMap<String, String> numberMap = new HashMap<>();
+            numberMap.put("one", "1");
+            numberMap.put("two", "2");
+            numberMap.put("three", "3");
+            numberMap.put("four", "4");
+            numberMap.put("five", "5");
+            numberMap.put("six", "6");
+            numberMap.put("seven", "7");
+            numberMap.put("eight", "8");
+            numberMap.put("nine", "9");
+        
+        List<Integer> numbers = new ArrayList<>();
+        
+        Pattern pattern = Pattern.compile("(\\d)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)");
+        for (String line : output) {
+            Matcher matcher = pattern.matcher(line);
+            List<String> result = new ArrayList<String>();       
+            int lastIndex = 0;     
+            while(matcher.find(lastIndex)){
+                result.add(matcher.group());
+                lastIndex = matcher.start() >= matcher.end() ? matcher.end() : lastIndex+1;
+            }
+            String first = numberMap.containsKey(result.get(0)) ? numberMap.get(result.get(0)) : result.get(0);
+            String last = numberMap.containsKey(result.get(result.size()-1)) ? numberMap.get(result.get(result.size()-1)) : result.get(result.size()-1);
+            numbers.add(Integer.valueOf(first + "" + last));
+        }
         return numbers;
     }
 }
