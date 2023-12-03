9c7eb5853e7c47a0dac84702e2a441f488ceb169
Oliver Vier <119545999+OliverVier@users.noreply.github.com>
2023-12-03 04:50:59 +0100

    refactor: renamed variables, more use of classses

--- aoc2023/src/main/java/de/o4/day_2/Day2.java
+++ aoc2023/src/main/java/de/o4/day_2/Day2.java
@@ -15,47 +15,46 @@ public class Day2 {
     public static void main(String[] args) {
         Day2 main = new Day2();
         List<String> output = TextReader.read("aoc2023\\src\\main\\java\\de\\o4\\day_2\\puzzle_input_2.txt");
+
         System.out.printf("Possible games: %s\n", main.sumOfPossibleGamesID(main.getGames(output)));
         System.out.printf("Sum of the power of sets: %s\n", main.sumOfPowOfCubes(main.getGames(output)));
     }
 
-    public List<List<HashMap<String, Integer>>> getGames(List<String> output) {
+    public List<Game> getGames(List<String> output) {
+        List<Game> games = new ArrayList<>();
 
-        List<List<HashMap<String, Integer>>> games = new ArrayList<>();
-        List<HashMap<String, Integer>> sets = new ArrayList<>();
-        HashMap<String, Integer> cubes;
+        for (String gameString : output) {
+            Game game = new Game();
+            String[] rounds = gameString.split(":")[1].split(";");
 
-        for (String game : output) { // Game
-            cubes = new HashMap<>();
-            game = game.split(":")[1];
-            String[] rounds = game.split(";");
-            sets = new ArrayList<>();
-            for (String round : rounds) { // Set
-                String[] cubeString = round.split(",");
-                for (String cube : cubeString) { // Würfel
+            for (String round : rounds) {
+                CubeSet set = new CubeSet();
+                String[] cubesString = round.split(",");
+
+                for (String cube : cubesString) {
                     String[] values = cube.split(" ");
-                    cubes.put(values[2], Integer.valueOf(values[1]));
+                    set.cubes.put(values[2], Integer.valueOf(values[1]));
                 }
-                sets.add(cubes);
-                cubes = new HashMap<String, Integer>();
+                game.sets.add(set);
             }
-            games.add(sets);
+            games.add(game);
         }
         return games;
     }
 
-    public int sumOfPossibleGamesID(List<List<HashMap<String, Integer>>> games) {
+    public int sumOfPossibleGamesID(List<Game> games) {
 
         int possibleGamesSumID = 0;
         int index = 0;
         boolean invalid = false;
 
-        for (List<HashMap<String, Integer>> game : games) {
+        for (Game game : games) {
             index++;
-            for (HashMap<String, Integer> set : game) {
-                if (set.getOrDefault("red", 0) > MAX_RED_CUBES ||
-                        set.getOrDefault("blue", 0) > MAX_BLUE_CUBES ||
-                        set.getOrDefault("green", 0) > MAX_GREEN_CUBES) {
+            for (CubeSet set : game.sets) {
+                HashMap<String, Integer> cubes = set.cubes;
+                if (cubes.getOrDefault("red", 0) > MAX_RED_CUBES ||
+                        cubes.getOrDefault("blue", 0) > MAX_BLUE_CUBES ||
+                        cubes.getOrDefault("green", 0) > MAX_GREEN_CUBES) {
                     invalid = true;
                     break;
                 }
@@ -64,39 +63,39 @@ public class Day2 {
                 invalid = false;
                 continue;
             }
-            possibleGamesSumID+=index;
+            possibleGamesSumID += index;
         }
         return possibleGamesSumID;
     }
 
-    public int sumOfPowOfCubes(List<List<HashMap<String, Integer>>> games) {
-
+    public int sumOfPowOfCubes(List<Game> games) {
         int sumOfPowOfCubes = 0;
 
-
-        for (List<HashMap<String, Integer>> game : games) {
+        for (Game game : games) {
             int maxBlue = 0, maxRed = 0, maxGreen = 0;
-            for (HashMap<String, Integer> set : game) {
-                if(set.getOrDefault("red", 0) > maxRed){
-                    maxRed = set.getOrDefault("red", 0);
+
+            for (CubeSet set : game.sets) {
+                HashMap<String, Integer> cubes = set.cubes;
+                if (cubes.getOrDefault("red", 0) > maxRed) {
+                    maxRed = cubes.getOrDefault("red", 0);
                 }
-                if(set.getOrDefault("blue", 0) > maxBlue){
-                    maxBlue = set.getOrDefault("blue", 0);
+                if (cubes.getOrDefault("blue", 0) > maxBlue) {
+                    maxBlue = cubes.getOrDefault("blue", 0);
                 }
-                if(set.getOrDefault("green", 0) > maxGreen){
-                    maxGreen = set.getOrDefault("green", 0);
+                if (cubes.getOrDefault("green", 0) > maxGreen) {
+                    maxGreen = cubes.getOrDefault("green", 0);
                 }
             }
-            sumOfPowOfCubes+=(maxBlue*maxGreen*maxRed);
+            sumOfPowOfCubes += (maxBlue * maxGreen * maxRed);
         }
         return sumOfPowOfCubes;
     }
 }
 
 class Game {
-    List<Set> set = new ArrayList<Set>();
+    List<CubeSet> sets = new ArrayList<CubeSet>();
 }
 
-class Set {
+class CubeSet {
     HashMap<String, Integer> cubes = new HashMap<String, Integer>();
 }
